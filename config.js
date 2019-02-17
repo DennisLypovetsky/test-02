c0f5c88c44aab19da932018ff6a722641ffe5721
Dennis Lypovetsky <tsssssssss@gmail.com>
2019-02-18 00:19:58 +0200

    Сonnects fonts

--- config.js
+++ config.js
@@ -14,7 +14,8 @@ let config = {
   ],
   "addStyleBefore": [
     "src/scss/variables.scss",
-    "src/scss/mixins.scss"
+    "src/scss/mixins.scss",
+    "src/scss/fonts.scss"
   ],
   "addStyleAfter": [
     // "src/scss/print.scss"
@@ -24,7 +25,18 @@ let config = {
     "./script.js"
   ],
   "addAssets": {
-    "src/fonts/demo-empty-open-sans.woff2": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-300.woff": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-300.woff2": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-700.woff": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-700.woff2": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-900.woff": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-900.woff2": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-regular.woff": "fonts/",
+    "src/fonts/montserrat-v12-cyrillic_latin-regular.woff2": "fonts/",
+    "src/fonts/open-sans-v15-cyrillic_latin-700.woff": "fonts/",
+    "src/fonts/open-sans-v15-cyrillic_latin-700.woff2": "fonts/",
+    "src/fonts/open-sans-v15-cyrillic_latin-regular.woff": "fonts/",
+    "src/fonts/open-sans-v15-cyrillic_latin-regular.woff2": "fonts/",
     "src/img/demo-*.{png,svg,jpg,jpeg}": "img/",
     // "src/favicon/*.{png,ico,svg,xml,webmanifest}": "img/favicon",
   },
